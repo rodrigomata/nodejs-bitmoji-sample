aea696c356640920245814d89460fa08aae257ab
Rodrigo Mata <rodrigo.mata.ib@gmail.com>
2019-07-08 14:56:18 -0500

    feat: relationships between models and upsert

--- server/models/avatar.js
+++ server/models/avatar.js
@@ -1,14 +1,20 @@
 module.exports = Avatar => {
 
-    // :: Method that seeds the database with the presets
-    // Avatar.generate = async () => {
+    // :: TODO: Method that seeds the database with the presets
+    Avatar.generate = async () => {
 
-    // }
+    }
 
     // :: Public method that modifies an avatar and retrieves its info
     Avatar.render = async data => {
-        
         console.log('data', data)
-        return 
+        const { name } = data
+        let avatar
+        try {
+           avatar = await Avatar.upsertWithWhere({ where: { name }, data })
+        } catch (error) {
+            throw new Error(error)
+        }
+        return avatar
     }
 };
